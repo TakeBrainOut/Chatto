ebd6b64361bcf864c30831191fab891000b6560a
kirill_vershitsky <kirill.vershitsky@gmail.com>
2019-03-27 14:33:05 +0300

    - fix. for swift 5

--- Chatto/Source/ChatController/BaseChatViewController.swift
+++ Chatto/Source/ChatController/BaseChatViewController.swift
@@ -127,7 +127,7 @@ open class BaseChatViewController: UIViewController, UICollectionViewDataSource,
         self.collectionView.showsHorizontalScrollIndicator = false
         self.collectionView.allowsSelection = false
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
-        self.collectionView.autoresizingMask = UIViewAutoresizing()
+        self.collectionView.autoresizingMask = UIView.AutoresizingMask()
         self.view.addSubview(self.collectionView)
         self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .top, relatedBy: .equal, toItem: self.collectionView, attribute: .top, multiplier: 1, constant: 0))
         self.view.addConstraint(NSLayoutConstraint(item: self.view, attribute: .leading, relatedBy: .equal, toItem: self.collectionView, attribute: .leading, multiplier: 1, constant: 0))
@@ -151,7 +151,7 @@ open class BaseChatViewController: UIViewController, UICollectionViewDataSource,
     private var collectionViewBottomConstraint: NSLayoutConstraint!
     private func addInputViews() {
         self.inputContainer = UIView(frame: CGRect.zero)
-        self.inputContainer.autoresizingMask = UIViewAutoresizing()
+        self.inputContainer.autoresizingMask = UIView.AutoresizingMask()
         self.inputContainer.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(self.inputContainer)
         self.view.addConstraint(NSLayoutConstraint(item: self.inputContainer, attribute: .top, relatedBy: .greaterThanOrEqual, toItem: self.topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
